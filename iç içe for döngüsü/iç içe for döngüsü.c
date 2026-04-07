3448fe3da0cc9f7c852317c4d11d791de6bd6cbd
Zeynep Karakaya <karakaya.zeyno60@gmail.com>
2026-04-08 01:05:58 +0300

    Add files via upload

---
+++ iç içe for döngüsü/iç içe for döngüsü.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    int satir = 6;
+    for (i = 1; i<= satir; i++) {
+        for (j = 1; j<= i; j++) {
+           prinf("*");
+           }
+           printf("/n");
+           }
+           return 0;
+}
